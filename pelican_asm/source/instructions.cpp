054b9af4f0c084bbc6ba82e791d1a1dc835f6fb7
ED <Enrico.Dressler@analog.com>
2026-01-22 16:23:01 +0100

    Corrected copyright statements.

--- pelican_asm/source/instructions.cpp
+++ pelican_asm/source/instructions.cpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
-* Copyright (C) 2024 Analog Devices Inc. All Rights Reserved. This software is
-* proprietary & confidential to Analog Devices, Inc. and its licensors.
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
+* This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
 #include "instructions.h"
--- pelican_asm/source/pelican_asm.cpp
+++ pelican_asm/source/pelican_asm.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2024 Analog Devices Inc. All Rights Reserved.
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
 * This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
